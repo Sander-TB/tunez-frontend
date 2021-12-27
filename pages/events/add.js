1d27460f701d03e40906a089e55e1730407b62c8
Sander Byrkjeland <sunbyr@gmail.com>
2021-12-27 23:38:42 +0100

    Add new event functionality + fix slugify. Delete event coming up

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,3 +1,5 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
@@ -18,9 +20,32 @@ export default function addEventPage() {
 
 	const router = useRouter();
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log(values);
+
+		// Validation
+		const hasEmptyFields = Object.values(values).some(
+			(element) => element === ""
+		);
+
+		if (hasEmptyFields) {
+			toast.error("Please fill in all fields");
+		}
+
+		const res = await fetch(`${API_URL}/events`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(values),
+		});
+
+		if (!res.ok) {
+			toast.error("Something went wrong");
+		} else {
+			const evt = await res.json();
+			router.push(`/events/${evt.slug}`);
+		}
 	};
 
 	const handleInputChange = (e) => {
@@ -34,6 +59,7 @@ export default function addEventPage() {
 				<a>Go Back</a>
 			</Link>
 			<h1>Add Event</h1>
+			<ToastContainer />
 			<form onSubmit={handleSubmit} className={styles.form}>
 				<div className={styles.grid}>
 					<div>
