107e71665ec4ab305bab45be0c54fb5e08cdb948
Sander Byrkjeland <sunbyr@gmail.com>
2022-01-04 20:28:47 +0100

    Made AuthContext, JWT is next

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import { RiMusic2Fill, RiLoginBoxLine, RiLogoutBoxLine } from "react-icons/ri";
+import { useContext } from "react";
 import Search from "./Search";
 import Link from "next/link";
+import AuthContext from "@/context/AuthContext";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
+	const { user, logout } = useContext(AuthContext);
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo}>
@@ -24,18 +28,39 @@ export default function Header() {
 							<a>Events</a>
 						</Link>
 					</li>
-					<li>
-						<Link href='/events/add'>
-							<a>Add Event</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/account/login'>
-							<a className='btn-secondary btn-icon'>
-								<RiLoginBoxLine /> Log In
-							</a>
-						</Link>
-					</li>
+					{user ? (
+						// If logged in
+						<>
+							<li>
+								<Link href='/events/add'>
+									<a>Add Event</a>
+								</Link>
+							</li>
+							<li>
+								<Link href='/account/dashboard'>
+									<a>Dashboard</a>
+								</Link>
+							</li>
+							<li>
+								<button
+									onClick={() => logout}
+									className='btn-secondary btn-icon'>
+									<RiLogoutBoxLine /> Log Out
+								</button>
+							</li>
+						</>
+					) : (
+						// If logged out
+						<>
+							<li>
+								<Link href='/account/login'>
+									<a className='btn-secondary btn-icon'>
+										<RiLoginBoxLine /> Log In
+									</a>
+								</Link>
+							</li>
+						</>
+					)}
 				</ul>
 			</nav>
 		</header>
